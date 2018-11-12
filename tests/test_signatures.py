44ef019772088f1495f21f6cc706d321a4bc11a2
Ibrakov Azat <azatibrakov@gmail.com>
2018-11-13 00:12:31 +0300

    Complete test

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -1,4 +1,5 @@
 import platform
+from itertools import chain
 from types import (BuiltinFunctionType,
                    FunctionType,
                    MethodType)
@@ -43,11 +44,24 @@ def test_factory_fail(unsupported_callable: Callable[..., Any]) -> None:
 def test_has_unset_parameters(callable_: Callable[..., Any]) -> None:
     result = signatures.factory(callable_)
 
-    assert (not isinstance(result, signatures.Plain)
-            or result.has_unset_parameters()
-            or all(parameter.has_default
-                   for parameter in result.parameters)
-            or result.parameters_by_kind[
-                signatures.Parameter.Kind.VARIADIC_POSITIONAL]
-            or result.parameters_by_kind[
-                signatures.Parameter.Kind.VARIADIC_KEYWORD])
+    if isinstance(result, signatures.Plain):
+        assert (result.has_unset_parameters()
+                or is_plain_signature_all_set(result))
+    else:
+        assert isinstance(result, signatures.Overloaded)
+        assert (result.has_unset_parameters()
+                or all(map(is_plain_signature_all_set, result.signatures)))
+
+
+def is_plain_signature_all_set(signature: signatures.Plain) -> bool:
+    variadic_parameters_kinds = {signatures.Parameter.Kind.VARIADIC_POSITIONAL,
+                                 signatures.Parameter.Kind.VARIADIC_KEYWORD}
+    non_variadic_parameters_kinds = (set(signatures.Parameter.Kind)
+                                     - variadic_parameters_kinds)
+    non_variadic_parameters = chain.from_iterable(
+            signature.parameters_by_kind[kind]
+            for kind in non_variadic_parameters_kinds)
+    return (all(parameter.has_default
+                for parameter in non_variadic_parameters)
+            or all(parameter.kind in variadic_parameters_kinds
+                   for parameter in signature.parameters))
