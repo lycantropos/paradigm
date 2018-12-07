57a03efe4133b00bee270349141329e5ee0a8471
Ibrakov Azat <azatibrakov@gmail.com>
2018-12-07 20:02:59 +0300

    Make 'test_signatures' module a package

---
+++ tests/signatures_tests/test_factory.py
@@ -0,0 +1,36 @@
+import platform
+from types import (BuiltinFunctionType,
+                   FunctionType,
+                   MethodType)
+from typing import (Any,
+                    Callable)
+
+import pytest
+
+from paradigm import signatures
+from paradigm.hints import (MethodDescriptorType,
+                            WrapperDescriptorType)
+
+
+def test_basic(built_in_function: BuiltinFunctionType,
+               class_: type,
+               function: FunctionType,
+               method: MethodType,
+               method_descriptor: MethodDescriptorType,
+               wrapper_descriptor: WrapperDescriptorType) -> None:
+    for callable_ in (built_in_function,
+                      class_,
+                      function,
+                      method,
+                      method_descriptor,
+                      wrapper_descriptor):
+        result = signatures.factory(callable_)
+
+        assert isinstance(result, signatures.Base)
+
+
+@pytest.mark.skipif(platform.python_implementation() == 'PyPy',
+                    reason='requires CPython')
+def test_fail(unsupported_callable: Callable[..., Any]) -> None:
+    with pytest.raises(ValueError):
+        signatures.factory(unsupported_callable)
