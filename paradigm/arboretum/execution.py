d6ecd31b429da0f6326385f8611603131d76fd70
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-08 15:02:24 +0300

    Complete 'If' node processing

--- paradigm/arboretum/execution.py
+++ paradigm/arboretum/execution.py
@@ -1,4 +1,5 @@
 from functools import singledispatch
+from pathlib import Path
 
 from typed_ast import ast3
 
@@ -10,6 +11,7 @@ from .hints import Namespace
 @singledispatch
 def execute(node: ast3.AST,
             *,
+            source_path: Path,
             namespace: Namespace) -> None:
     raise TypeError('Unsupported node type: {type}.'
                     .format(type=type(node)))
@@ -18,14 +20,17 @@ def execute(node: ast3.AST,
 @execute.register(ast3.stmt)
 def execute_statement(node: ast3.stmt,
                       *,
+                      source_path: Path,
                       namespace: Namespace) -> None:
     execute_tree(construction.from_node(node),
+                 source_path=source_path,
                  namespace=namespace)
 
 
 @execute.register(ast3.Module)
 def execute_tree(node: ast3.Module,
                  *,
+                 source_path: Path,
                  namespace: Namespace) -> None:
-    code = compile(conversion.typed_to_plain(node), '<unknown>', 'exec')
+    code = compile(conversion.typed_to_plain(node), str(source_path), 'exec')
     exec(code, namespace)
