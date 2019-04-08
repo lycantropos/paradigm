0632f09c0b1a055258c7050a9dba46e2b716d930
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-08 15:24:46 +0300

    Complete test for signatures '__str__' method

--- tests/signatures_tests/test_str.py
+++ tests/signatures_tests/test_str.py
@@ -5,3 +5,10 @@ def test_type(signature: signatures.Base) -> None:
     result = str(signature)
 
     assert isinstance(result, str)
+
+
+def test_nesting(overloaded_signature: signatures.Overloaded) -> None:
+    result = str(overloaded_signature)
+
+    assert all(str(signature) in result
+               for signature in overloaded_signature.signatures)
