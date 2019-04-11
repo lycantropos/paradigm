e19dadc00d12a81f6a70bb88e405e6abf27b946d
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-11 20:41:06 +0300

    Remove tests for 'arboretum.namespaces' module

--- tests/namespaces_tests/test_from_module_path.py
+++
@@ -1,8 +0,0 @@
-from paradigm import catalog
-from paradigm.arboretum.namespaces import from_module_path
-
-
-def test_basic(module_path: catalog.Path) -> None:
-    result = from_module_path(module_path)
-
-    assert result['__name__'] == str(module_path)
