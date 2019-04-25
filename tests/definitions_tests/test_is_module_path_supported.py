909a3cf0b0fe042b9edaf9dafe972a6651333367
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-25 19:42:47 +0300

    Complete strategies & fixtures

--- tests/definitions_tests/test_is_module_path_supported.py
+++ tests/definitions_tests/test_is_module_path_supported.py
@@ -3,8 +3,8 @@ from hypothesis import given
 from paradigm import catalog
 from paradigm.definitions.base import (find_spec,
                                        is_module_path_supported)
-from tests import strategies
 from tests.utils import implication
+from . import strategies
 
 
 @given(strategies.modules_paths)
