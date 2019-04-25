fe467cfad21d39487a6411595afbb62f0a161cfe
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-25 19:23:15 +0300

    Complete fixtures

--- tests/catalog_tests/test_is_attribute.py
+++ tests/catalog_tests/test_is_attribute.py
@@ -1,7 +1,11 @@
+from hypothesis import given
+
 from paradigm import catalog
+from tests import strategies
 from tests.utils import equivalence
 
 
+@given(strategies.objects_paths)
 def test_relation_to_parent(object_path: catalog.Path) -> None:
     assert equivalence(catalog.is_attribute(object_path),
                        len(object_path.parent.parts) > 0)
--- tests/definitions_tests/test_is_module_path_supported.py
+++ tests/definitions_tests/test_is_module_path_supported.py
@@ -1,9 +1,13 @@
+from hypothesis import given
+
 from paradigm import catalog
 from paradigm.definitions.base import (find_spec,
                                        is_module_path_supported)
+from tests import strategies
 from tests.utils import implication
 
 
-def test_relation_with_find_spec(object_path: catalog.Path) -> None:
-    assert implication(is_module_path_supported(object_path),
-                       find_spec(object_path) is not None)
+@given(strategies.modules_paths)
+def test_relation_with_find_spec(module_path: catalog.Path) -> None:
+    assert implication(is_module_path_supported(module_path),
+                       find_spec(module_path) is not None)
