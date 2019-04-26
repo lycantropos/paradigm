981e30d970b7beac64305b2757542ed6ef5ff487
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-26 11:23:33 +0300

    Complete test for 'is_module_path_supported' predicate

--- tests/definitions_tests/test_is_module_path_supported.py
+++ tests/definitions_tests/test_is_module_path_supported.py
@@ -2,7 +2,8 @@ from hypothesis import given
 
 from paradigm import catalog
 from paradigm.definitions.base import (find_spec,
-                                       is_module_path_supported)
+                                       is_module_path_supported,
+                                       stdlib_modules_names)
 from tests.utils import implication
 from . import strategies
 
@@ -10,4 +11,5 @@ from . import strategies
 @given(strategies.modules_paths)
 def test_relation_with_find_spec(module_path: catalog.Path) -> None:
     assert implication(is_module_path_supported(module_path),
-                       find_spec(module_path) is not None)
+                       str(module_path) in stdlib_modules_names
+                       or find_spec(module_path) is not None)
