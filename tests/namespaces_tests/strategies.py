a6a5853be885f8b26620c9d9cf03a51a4da0c9c8
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-03 20:06:15 +0700

    Add tests for 'namespaces' module

---
+++ tests/namespaces_tests/strategies.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+from hypothesis import strategies
+
+from paradigm import catalog
+from paradigm.hints import Namespace
+from paradigm.namespaces import from_module
+from tests.strategies import (modules,
+                              non_empty_objects_paths)
+from tests.utils import Strategy
+
+modules_paths = modules.map(catalog.from_module)
+namespaces = modules.map(from_module)
+
+
+def to_namespaces_with_non_empty_objects_paths(
+        namespace: Namespace) -> Strategy[Tuple[Namespace, catalog.Path]]:
+    return strategies.tuples(strategies.just(namespace),
+                             strategies.sampled_from([catalog.from_string(name)
+                                                      for name in namespace])
+                             | non_empty_objects_paths)
+
+
+namespaces_with_non_empty_objects_paths = namespaces.flatmap(
+        to_namespaces_with_non_empty_objects_paths)
--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -1,3 +1,5 @@
+from .catalog import (non_empty_objects_paths,
+                      objects_paths)
 from .utils import (identifiers,
                     invalid_sources,
                     modules,
