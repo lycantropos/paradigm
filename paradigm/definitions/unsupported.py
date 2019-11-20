2995192d7f8e9f7dbf0f5650adf9bd0a598eebe5
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-20 14:06:57 +0300

    Complete unsupported objects

--- paradigm/definitions/unsupported.py
+++ paradigm/definitions/unsupported.py
@@ -58,13 +58,6 @@ built_in_functions = set()
 
 if platform.python_implementation() != 'PyPy':
     # not supported by ``typeshed`` package
-    _update(built_in_functions, '_hashlib', ['openssl_md5',
-                                             'openssl_sha1',
-                                             'openssl_sha1',
-                                             'openssl_sha224',
-                                             'openssl_sha256',
-                                             'openssl_sha384',
-                                             'openssl_sha512'])
     _add(built_in_functions, '_json', 'encode_basestring')
     _update(built_in_functions, '_thread', ['allocate',
                                             'exit_thread',
@@ -77,17 +70,45 @@ if platform.python_implementation() != 'PyPy':
                                            'xmlcharrefreplace_errors'])
     _update(built_in_functions, 'ctypes', ['_dlopen', 'pointer'])
     _add(built_in_functions, 'socket', 'dup')
-    _update(built_in_functions, 'sys', ['callstats',
-                                        'getallocatedblocks',
-                                        'get_coroutine_wrapper',
-                                        'set_coroutine_wrapper'])
+    if sys.version_info < (3, 8):
+        _update(built_in_functions, '_hashlib', ['openssl_md5',
+                                                 'openssl_sha1',
+                                                 'openssl_sha224',
+                                                 'openssl_sha256',
+                                                 'openssl_sha384',
+                                                 'openssl_sha512'])
+        _update(built_in_functions, 'sys', ['callstats',
+                                            'getallocatedblocks',
+                                            'get_coroutine_wrapper',
+                                            'set_coroutine_wrapper'])
+    else:
+        _add(built_in_functions, '_thread', 'get_native_id')
+        _update(built_in_functions, '_xxsubinterpreters', ['channel_send',
+                                                           'get_current',
+                                                           'channel_recv',
+                                                           'is_running',
+                                                           'channel_create',
+                                                           'list_all',
+                                                           'channel_close',
+                                                           'channel_release',
+                                                           'destroy',
+                                                           'is_shareable',
+                                                           'get_main',
+                                                           'channel_destroy',
+                                                           'create',
+                                                           'channel_list_all',
+                                                           'run_string'])
     if sys.version_info >= (3, 6):
-        _update(built_in_functions, 'sys', ['getfilesystemencodeerrors',
-                                            'get_asyncgen_hooks',
-                                            'set_asyncgen_hooks'])
+        _add(built_in_functions, 'sys', 'set_asyncgen_hooks')
+        if sys.version_info < (3, 8):
+            _update(built_in_functions, 'sys', ['get_asyncgen_hooks',
+                                                'getfilesystemencodeerrors'])
 
     if sys.version_info >= (3, 7):
         _add(built_in_functions, 'socket', 'close')
+
+    if sys.version_info >= (3, 8):
+        _add(built_in_functions, 'sys', 'audit')
 
     if sys.platform == 'linux':
         _update(built_in_functions, '_locale', ['bind_textdomain_codeset',
@@ -99,6 +120,9 @@ if platform.python_implementation() != 'PyPy':
 
         if sys.version_info >= (3, 7):
             _add(built_in_functions, 'time', 'pthread_getcpuclockid')
+        if sys.version_info >= (3, 8):
+            _update(built_in_functions, 'posix', ['posix_spawn',
+                                                  'posix_spawnp'])
 
 classes = set()
 
@@ -111,14 +135,20 @@ if platform.python_implementation() != 'PyPy':
                              '_TextIOBase'])
     _add(classes, '_ssl', '_SSLContext')
     _update(classes, '_thread', ['RLock', '_local'])
-    _update(classes, 'itertools', ['_grouper', '_tee', '_tee_dataobject'])
     _add(classes, 'asyncio.events', '_RunningLoop')
     _add(classes, 'ctypes', '_CFuncPtr')
     _add(classes, 'encodings', 'CodecRegistryError')
-    _add(classes, 'random', '_MethodType')
+    _update(classes, 'itertools', ['_grouper', '_tee_dataobject'])
 
     if sys.version_info < (3, 7):
         _add(classes, '_collections_abc', 'range_iterator')
+    if sys.version_info < (3, 8):
+        _add(classes, 'itertools', '_tee')
+        _add(classes, 'random', '_MethodType')
+    else:
+        _add(classes, '_xxsubinterpreters', 'InterpreterID')
+        _add(classes, 'pickle', 'PickleBuffer')
+        _update(classes, 'types', ['CellType', 'MethodType'])
 
     if sys.platform == 'win32':
         _update(classes, 'msilib', ['UuidCreate',
@@ -162,6 +192,70 @@ if platform.python_implementation() != 'PyPy':
     else:
         _add(methods_descriptors, 'collections', 'OrderedDict.setdefault')
         _add(methods_descriptors, 'zipimport', 'zipimporter.find_loader')
+
+    if sys.version_info >= (3, 8):
+        _update(methods_descriptors, 'curses', ['window.addch',
+                                                'window.addnstr',
+                                                'window.addstr',
+                                                'window.border',
+                                                'window.box',
+                                                'window.chgat',
+                                                'window.clear',
+                                                'window.clearok',
+                                                'window.clrtobot',
+                                                'window.clrtoeol',
+                                                'window.cursyncup',
+                                                'window.delch',
+                                                'window.deleteln',
+                                                'window.derwin',
+                                                'window.erase',
+                                                'window.get_wch',
+                                                'window.getbegyx',
+                                                'window.getch',
+                                                'window.getkey',
+                                                'window.getmaxyx',
+                                                'window.getparyx',
+                                                'window.getstr',
+                                                'window.getyx',
+                                                'window.hline',
+                                                'window.idcok',
+                                                'window.idlok',
+                                                'window.immedok',
+                                                'window.inch',
+                                                'window.insch',
+                                                'window.insdelln',
+                                                'window.insertln',
+                                                'window.insnstr',
+                                                'window.insstr',
+                                                'window.instr',
+                                                'window.is_wintouched',
+                                                'window.keypad',
+                                                'window.leaveok',
+                                                'window.move',
+                                                'window.mvderwin',
+                                                'window.mvwin',
+                                                'window.nodelay',
+                                                'window.notimeout',
+                                                'window.noutrefresh',
+                                                'window.overlay',
+                                                'window.overwrite',
+                                                'window.redrawwin',
+                                                'window.refresh',
+                                                'window.resize',
+                                                'window.scroll',
+                                                'window.scrollok',
+                                                'window.standend',
+                                                'window.standout',
+                                                'window.subpad',
+                                                'window.subwin',
+                                                'window.syncdown',
+                                                'window.syncok',
+                                                'window.syncup',
+                                                'window.timeout',
+                                                'window.touchline',
+                                                'window.touchwin',
+                                                'window.untouchwin',
+                                                'window.vline'])
 
     if sys.platform == 'win32':
         _add(methods_descriptors, 'socket', 'socket.share')
