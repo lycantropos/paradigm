140363967c68f43083f5e76d349722355a335577
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-14 05:01:04 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@ from hypothesis import (HealthCheck,
 
 settings.register_profile('default',
                           deadline=None,
-                          suppress_health_check=[HealthCheck.filter_too_much,
+                          suppress_health_check=[HealthCheck.data_too_large,
+                                                 HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
