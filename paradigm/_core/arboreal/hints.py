0bc704a65787240151fde96849190aedcdd7ec9f
Azat Ibrakov <azatibrakov@gmail.com>
2022-10-29 13:57:36 +0200

    Remove redundant module

--- paradigm/_core/arboreal/hints.py
+++
@@ -1,9 +0,0 @@
-import ast
-from typing import (Dict,
-                    List,
-                    Union)
-
-from paradigm._core import catalog
-
-Node = Union[ast.AST, catalog.Path]
-Scope = Dict[catalog.Path, List[Node]]
