7beee7754b8f861c74f538683462d8eabbe0d688
Azat Ibrakov <azatibrakov@gmail.com>
2022-11-09 00:48:19 +0100

    Complete test of 'signature_from_callable' function

--- tests/base_tests/test_signature_from_callable.py
+++ tests/base_tests/test_signature_from_callable.py
@@ -3,6 +3,8 @@ from typing import (Any,
 
 from hypothesis import given
 
+from paradigm._core import catalog
+from paradigm._core.sources import stdlib_modules_paths
 from paradigm.base import (OverloadedSignature,
                            PlainSignature,
                            signature_from_callable)
@@ -18,6 +20,9 @@ def test_basic(callable_: Callable[..., Any]) -> None:
 
 @given(strategies.overloaded_callables)
 def test_overloaded(callable_: Callable[..., Any]) -> None:
+    module_path, _ = catalog.qualified_path_from(callable_)
+    assert module_path in stdlib_modules_paths, module_path
+
     result = signature_from_callable(callable_)
 
     assert isinstance(result, OverloadedSignature)
