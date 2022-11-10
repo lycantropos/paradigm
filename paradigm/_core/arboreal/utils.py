1666c23cd6bc9ab4439ada258cdce20d4bdc732c
Azat Ibrakov <azatibrakov@gmail.com>
2022-11-10 23:26:46 +0100

    Fix 'mypy' errors

--- paradigm/_core/arboreal/utils.py
+++ paradigm/_core/arboreal/utils.py
@@ -1,37 +1,36 @@
-import functools as _functools
-import sys as _sys
-import typing as _t
+import functools
+import sys
+import typing as t
 
-singledispatchmethod: _t.Any
-
-if _sys.version_info >= (3, 8):
-    singledispatchmethod = _functools.singledispatchmethod
-else:
-    _T1 = _t.TypeVar('_T1')
-    _T2 = _t.TypeVar('_T2')
+singledispatchmethod: t.Any
+if sys.version_info < (3, 8):
+    _T1 = t.TypeVar('_T1')
+    _T2 = t.TypeVar('_T2')
 
 
     class _singledispatchmethod:
-        dispatcher: _t.Any
-        func: _t.Callable[..., _T1]
+        dispatcher: t.Any
+        func: t.Callable[..., _T1]
 
         def __new__(cls,
-                    func: _t.Callable[..., _T1]) -> '_singledispatchmethod':
+                    func: t.Callable[
+                        ..., _T1]) -> '_singledispatchmethod':
             if not callable(func) and not hasattr(func, '__get__'):
                 raise TypeError(f'{func!r} is not callable or a descriptor')
             self = super().__new__(cls)
-            self.dispatcher, self.func = _functools.singledispatch(func), func
+            self.dispatcher, self.func = functools.singledispatch(func), func
             return self
 
-        @_t.overload
+        @t.overload
         def register(self,
-                     cls: _t.Type,
-                     method: _t.Callable[..., _T1]) -> _t.Callable[..., _T1]:
+                     cls: t.Type,
+                     method: t.Callable[..., _T1]) -> t.Callable[
+            ..., _T1]:
             return self.dispatcher.register(cls, method)
 
-        @_t.overload
-        def register(self, cls: _t.Type) -> _t.Callable[
-            [_t.Callable[..., _T1]], _t.Callable[..., _T1]
+        @t.overload
+        def register(self, cls: t.Type) -> t.Callable[
+            [t.Callable[..., _T1]], t.Callable[..., _T1]
         ]:
             return self.dispatcher.register(cls)
 
@@ -40,15 +39,17 @@ else:
 
         def __get__(self,
                     instance: _T2,
-                    cls: _t.Optional[_T2] = None) -> _t.Callable[..., _T1]:
-            def dispatchable_method(*args: _t.Any, **kwargs: _t.Any) -> _t.Any:
+                    cls: t.Optional[_T2] = None) -> t.Callable[
+            ..., _T1]:
+            def dispatchable_method(*args: t.Any,
+                                    **kwargs: t.Any) -> t.Any:
                 method = self.dispatcher.dispatch(args[0].__class__)
                 return method.__get__(instance, cls)(*args, **kwargs)
 
-            result: _t.Any = dispatchable_method
+            result: t.Any = dispatchable_method
             result.__isabstractmethod__ = self.__isabstractmethod__
             result.register = self.register
-            _functools.update_wrapper(result, self.func)
+            functools.update_wrapper(result, self.func)
             return result
 
         @property
@@ -57,3 +58,5 @@ else:
 
 
     singledispatchmethod = _singledispatchmethod
+else:
+    singledispatchmethod = functools.singledispatchmethod
