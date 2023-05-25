adad405bc1c95a54443acc6a9d25a34ed9176bc5
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-25 08:14:39 +0200

    Complete metadata

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
-from pathlib import Path
-
 from setuptools import (find_packages,
                         setup)
 
-import paradigm
-
 project_base_url = 'https://github.com/lycantropos/paradigm/'
 
-setup(name=paradigm.__name__,
-      packages=find_packages(exclude=('tests', 'tests.*')),
-      package_data={paradigm.__name__: ['py.typed']},
-      version=paradigm.__version__,
-      description=paradigm.__doc__,
-      long_description=Path('README.md').read_text(encoding='utf-8'),
-      long_description_content_type='text/markdown',
-      author='Azat Ibrakov',
-      author_email='azatibrakov@gmail.com',
-      classifiers=[
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-      ],
-      license='MIT License',
+
+setup(packages=find_packages(exclude=('tests', 'tests.*')),
       url=project_base_url,
-      download_url=project_base_url + 'archive/master.zip',
-      python_requires='>=3.7',
-      install_requires=Path('requirements.txt').read_text(encoding='utf-8'))
+      download_url=project_base_url + 'archive/master.zip')
